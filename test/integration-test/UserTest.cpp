552f3d59af27122afaf93e7a6d935a31830b13c6
gumb0 <amaiboroda@gmail.com>
2014-08-30 19:38:32 +0400

    Test of Client::findUserById

--- test/integration-test/UserTest.cpp
+++ test/integration-test/UserTest.cpp
@@ -6,19 +6,20 @@
 using namespace Instagram;
 using namespace testing;
 
-class UserTest : public Test
-{
-    virtual void SetUp()
-    {
-//        userInfo = { "id", "userName", "full name", "profile pic", "bio", "website", { 1, 2, 3 } };
-//        user.reset(new UserImpl(userInfo));
-    }
-
-protected:
-};
-
-TEST_F(UserTest, getsId)
+TEST(UserTest, findsUserById)
 {
     ClientPtr client = CreateClient("d7b1086490044ada88da482a87b073f3");
+
+    UserPtr user = client->findUserById("1479058533");
+
+    EXPECT_THAT(user->getId(), StrEq("1479058533"));
+    EXPECT_THAT(user->getUsername(), StrEq("coconut_beard"));
+    EXPECT_THAT(user->getFullName(), StrEq("CB"));
+    EXPECT_THAT(user->getProfilePicture(), StrEq("http://photos-c.ak.instagram.com/hphotos-ak-xaf1/10654924_727813450634146_619431088_a.jpg"));
+    EXPECT_THAT(user->getBio(), StrEq("I'm bearded."));
+    EXPECT_THAT(user->getWebsite(), StrEq("http://en.wikipedia.org/wiki/Beard"));
+    EXPECT_THAT(user->getMediaCount(), 2);
+    EXPECT_THAT(user->getFollowsCount(), 2);
+    EXPECT_THAT(user->getFollowedByCount(), 1);
 }
 

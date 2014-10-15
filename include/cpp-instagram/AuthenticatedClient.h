3b72af2a359a2eb6be86a1a370ca107b4f71ddc9
gumb0 <amaiboroda@gmail.com>
2014-10-15 20:11:05 +0400

    Fix misspelled word

--- include/cpp-instagram/AuthenticatedClient.h
+++ include/cpp-instagram/AuthenticatedClient.h
@@ -12,7 +12,7 @@ namespace Instagram
     class AuthenticatedClient : public Client
     {
     public:
-        // 0 passed to cout will make it ignored
+        // 0 passed to count will make it ignored
         // empty string passed to minId/maxId makes them ignored
         virtual Feed getFeed(int count = 0, const std::string& minId = std::string(), const std::string& maxId = std::string()) const = 0;
     };

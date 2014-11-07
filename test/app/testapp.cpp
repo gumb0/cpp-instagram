c5a4cc5392ebc6f9cb4d31479dfe57e3c121e013
romeodeepmind <romeo.deepmind@gmail.com>
2014-11-08 02:04:42 +0400

    demo app changed to serve for popular image download and tag management

--- test/app/testapp.cpp
+++ test/app/testapp.cpp
@@ -6,57 +6,68 @@
  */
 #include <iostream>
 
-#include "cpp-instagram/AuthenticatedClient.h"
-#include "cpp-instagram/Exception.h"
+#include "cpp-instagram/Instagram.h"
 
 #include<boost/program_options.hpp>
 
 using namespace Instagram;
 using namespace std;
 
-void downloadPopularImages(string token)
-{
-    try
-    {
-        ClientPtr client = CreateClient(token);
+void downloadImages(string client_id, string tag) {
+	try {
+		ClientPtr client = CreateClient(client_id);
 
-        MediaList medias = client->getPopularMedias();
+		cout << "Connecting to Instagram with client-id " << client_id << endl;
 
-        for (MediaPtr media : medias)
-        {
-            const std::string id = media->getId();
-            media->getImages()->getStandardResolution()->download(id + ".jpg");
-        }
-    }
-    catch (const Exception& e)
-    {
-        std::cerr << "Exception raised: " << e.getMessage() << std::endl;
-    }
+
+		MediaList medias;
+		if(!tag.empty()){
+			cout << "Searching for recent images for tag " << tag << endl;
+			medias = client->getRecentMediasByTag(tag);
+		}else{
+			medias = client->getPopularMedias();
+			cout << "Searching for recent popular images" << endl;
+		}
+
+		cout << "Found " << medias.size() << " photos" << endl;
+		for (MediaPtr media : medias) {
+			const string id = media->getId();
+			cout << "Downloading image with id" << id << endl;
+			media->getImages()->getStandardResolution()->download(id + ".jpg");
+		}
+	} catch (const Exception& e) {
+		cerr << "Exception raised: " << e.getMessage() << endl;
+	}
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
 
 	namespace po = boost::program_options;
 	po::options_description desc("Allowed options");
 	desc.add_options()
-	("help", "describe arguments")
-	("token", po::value<string>(), "token");
+			("help", "describe arguments")
+			("client-id", po::value<string>(), "Client id for user app")
+			("tag", po::value<string>(), "tag to search");
 
 	po::variables_map vm;
 	po::store(po::parse_command_line(argc, argv, desc), vm);
 	po::notify(vm);
 
 	if (vm.count("help")) {
-	cout << desc << "\n";
-	return 1;
+		cout << desc << endl;
+		return 1;
 	}
 
-	if (vm.count("token")) {
-		cout << "Token is " << vm["token"].as<string>() << ".\n";
-		downloadPopularImages(vm["token"].as<string>());
-	} else {
-		cout << "token is not set!\n";
+	if (!vm.count("client-id")) {
+		std::cerr << "token is not set!\n";
+		cout << desc << endl;
+		return 1;
 	}
+
+	string token = vm["client-id"].as<string>();
+	string tag = vm["tag"].as<string>();
+
+	downloadImages(token, tag);
 
 }
 
